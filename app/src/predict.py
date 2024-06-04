fcb039aa08830e9ef0274633f304cb791d5c8775
FabioSol <fabioso2231@gmail.com>
2024-06-04 14:40:18 -0600

    checkpoint

--- app/src/predict.py
+++
@@ -1,9 +0,0 @@
-import os.path
-import pickle
-from AidsModel import model_path
-
-def predict(X):
-    print(model_path)
-    with open(model_path, 'rb') as f:  # Replace 'model.pkl' with your filename
-        model = pickle.load(f)
-
